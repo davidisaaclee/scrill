c0c293e5b3fc087308d31274e53c7683348c22c6
David Lee <aewofij@gmail.com>
2016-07-11 08:57:50 -0400

    Arrange files; add basic selection functionality

--- script.js
+++ script.js
@@ -1,197 +1,4 @@
-function makeElement(options) {
-	var result = document.createElement(options.tag);
-
-	if (options.properties !== undefined) {
-		_.extend(result, options.properties);
-	}
-
-	return result;
-}
-
-function Document() {
-	_.extend(this, {
-		paragraphs: []
-	});
-}
-
-_.extend(Document.prototype, {
-	appendParagraph: function () {
-		this.paragraphs.push(new Paragraph());
-		return this;
-	},
-
-	appendAtom: function (atom) {
-		if (_.last(this.paragraphs) === undefined) {
-			this.appendParagraph();
-		}
-
-		_.last(this.paragraphs).appendAtom(atom);
-		return this;
-	}
-});
-
-
-
-function Paragraph() {
-	_.extend(this, {
-		atoms: []
-	});
-}
-
-Paragraph.prototype.appendAtom = function (atom) {
-	this.atoms.push(atom);
-	return this;
-};
-
-
-// function parse_inWord (document, workingString, text) {
-//	switch (_.head(text)) {
-//	case undefined:
-//		return document;
-
-//   case " ":
-//	  return parse_inSpace(document.appendAtom(workingString), text);
-
-//	case "\n":
-//	  return parse_inSpace(document.appendAtom(workingString), text);
-
-//	default:
-//		return parse_inWord(document, workingString + _.head(text), _.tail(text));
-//	}
-// }
-
-// function parse_inSpace (document, text) {
-//	switch (_.head(text)) {
-//	case undefined:
-//		return document;
-
-//   case " ":
-//	  return parse_inSpace(document.appendAtom(" "), _.tail(text));
-
-//	case "\n":
-//		return parse_inSpace(document.appendParagraph(), _.tail(text));
-
-//	default:
-//		return parse_inWord(document, "", text);
-//	}
-// }
-
-/*
-ParseResult ::= { state: ParseState
-								, document: Document
-								, input: [Character]
-								, workingString: String
-								}
-
-ParseState ::= "end" | "space" | "word"
-*/
-
-// (Document, String, [Character]) -> ParseResult
-function parse_inWord (document, workingString, text) {
-	switch (_.head(text)) {
-	case undefined:
-		return {
-			state: "end",
-			document: document
-		};
-
-  case " ":
-		return {
-			state: "space",
-			document: document.appendAtom(workingString),
-			input: text
-		};
-
-	case "\n":
-		return {
-			state: "space",
-			document: document.appendAtom(workingString),
-			input: text
-		};
-
-	default:
-		return {
-			state: "word",
-			document: document,
-			input: _.tail(text),
-			workingString: workingString + _.head(text)
-		};
-	}
-}
-
-// (Document, [Character]) -> ParseResult
-function parse_inSpace (document, text) {
-	switch (_.head(text)) {
-	case undefined:
-		return {
-			state: "end",
-			document: document
-		};
-
-  case " ":
-		return {
-			state: "space",
-			document: document.appendAtom(" "),
-			input: _.tail(text)
-		};
-
-	case "\n":
-		return {
-			state: "space",
-			document: document.appendParagraph(),
-			input: _.tail(text)
-		};
-
-	default:
-		return {
-			state: "word",
-			document: document,
-			input: text,
-			workingString: ""
-		};
-	}
-}
-
-function display (text, node) {
-	// var list = text.split("");
-	var result = parse_inSpace(new Document(), text.split(""));
-
-	while (result.state !== "end") {
-		switch (result.state) {
-			case "space":
-				result = parse_inSpace(result.document, result.input);
-				break;
-
-			case "word":
-				result = parse_inWord(result.document, result.workingString, result.input);
-				break;
-		}
-	}
-
-	var doc = result.document;
-
-	var html = doc.paragraphs.map(function (paragraph, paragraphIndex) {
-		var paragraphElement = makeElement({
-			tag: "p"
-		});
-
-		paragraph.atoms
-			// Make elements out of each atom.
-			.map(function (atom, atomIndex) {
-				return makeElement({
-					tag: "span",
-					properties: {
-						innerHTML: atom,
-						id: "atom-" + paragraphIndex + "-" + atomIndex
-					}
-				});
-			})
-			// Append each element onto the paragraph element.
-			.forEach(paragraphElement.appendChild, paragraphElement);
-
-		return paragraphElement;
-	}).forEach(node.appendChild, node);
-}
+// --- Retrieval --- //
 
 function requestText (path) {
 	request(path, "GET", function (responseText) {
@@ -211,16 +18,100 @@ function requestFileIndex () {
 	});
 }
 
-function request (path, method, success) {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function () {
-	  if (request.readyState == 4 && request.status == 200) {
-	    success(request.responseText);
-	  }
-	};
 
-	request.open(method, path, true);
-	request.send();
+// --- View --- //
+
+// (Int, Int) -> String
+function idForAtom (paragraphIndex, atomIndex) {
+	return "atom-" + paragraphIndex + "-" + atomIndex;
 }
+
+// AtomIndex ::= { paragraphIndex: Int, atomIndex: Int }
+
+// String -> AtomIndex
+function parseAtomID (atomID) {
+	var split = atomID.split("-");
+
+	return {
+		paragraphIndex: parseInt(split[1]),
+		atomIndex: parseInt(split[2])
+	};
+}
+
+// ComparisonResult ::= -1 | 0 | 1
+
+// (AtomIndex, AtomIndex) -> ComparisonResult
+// (ComparisonResult = 1 if leftAtom comes before rightAtom)
+function compareAtoms (leftAtom, rightAtom) {
+	if (leftAtom.paragraphIndex < rightAtom.paragraphIndex) {
+		return 1
+	} else if (leftAtom.paragraphIndex > rightAtom.paragraphIndex) {
+		return -1
+	} else {
+		if (leftAtom.atomIndex < rightAtom.atomIndex) {
+			return 1
+		} else if (leftAtom.atomIndex > rightAtom.atomIndex) {
+			return -1
+		} else {
+			return 0
+		}
+	}
+}
+
+function display (text, node) {
+	var doc = parse(text);
+
+	var html = doc.paragraphs.map(function (paragraph, paragraphIndex) {
+		var paragraphElement = makeElement({
+			tag: "p"
+		});
+
+		paragraph.atoms
+			// Make elements out of each atom.
+			.map(function (atom, atomIndex) {
+				return makeElement({
+					tag: "span",
+					properties: {
+						innerHTML: atom,
+						id: idForAtom(paragraphIndex, atomIndex)
+					}
+				});
+			})
+			// Append each element onto the paragraph element.
+			.forEach(paragraphElement.appendChild, paragraphElement);
+
+		return paragraphElement;
+	}).forEach(node.appendChild, node);
+}
+
+
+// --- Interaction --- //
+
+function handleSelection () {
+	var selection = window.getSelection();
+	var anchorIndices = parseAtomID(selection.anchorNode.parentElement.id);
+	var focusIndices = parseAtomID(selection.focusNode.parentElement.id);
+
+	var start, end;
+	if (compareAtoms(anchorIndices, focusIndices) < 0) {
+		start = focusIndices;
+		end = anchorIndices;
+	} else {
+		start = anchorIndices;
+		end = focusIndices;
+	}
+
+	for (var paragraphIdx = start.paragraphIndex; paragraphIdx <= end.paragraphIndex; paragraphIdx++) {
+		for (var atomIdx = start.atomIndex; atomIdx <= end.atomIndex; atomIdx++) {
+			var element = document.querySelector("#" + idForAtom(paragraphIdx, atomIdx));
+			element.classList.add("selected");
+		}
+	}
+}
+
+
+
+
+// --- Run --- //
 
 window.onload = requestFileIndex;
